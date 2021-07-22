6a61dc65bea31a696e53a339047ef11376218363
Daniel Thompson <dthompson0190@gmail.com>
2021-07-22 00:09:25 -0400

    deleted entire code base

--- login_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login),
-    path('user_login', views.user_login),
-    path('register', views.register),
-    path('signup', views.signup),
-]
--- profile_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProfileAppConfig(AppConfig):
-    name = 'profile_app'
